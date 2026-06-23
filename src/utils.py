c9dcf219f605cecc89a96086aade8458c0120a0d
abdubakr77 <abdubakora1232@gmail.com>
2026-06-23 18:46:28 +0300

    feat: add utility function for class distribution visualization

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,17 @@
+import matplotlib.pyplot as plt
 
+def show_count_plots(df,col,draw_pie_chart = True):
+    counts = df[col].value_counts()
+
+    fig, ax = plt.subplots(1, 2, figsize=(14, 6))
+
+    ax[0].barh(counts.index, counts.values, color='steelblue')
+    ax[0].set_xlabel('Count')
+    ax[0].set_title('Bar Chart Class Distribution')
+
+    if draw_pie_chart:
+        ax[1].pie(counts.values, labels=counts.index, autopct='%1.1f%%')
+        ax[1].set_title('Pie Chart Class Distribution')
+
+    plt.tight_layout()
+    plt.show()
