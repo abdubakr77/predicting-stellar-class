c1b91f35009e8271a90cd077c6ad28028788e8a3
abdubakr77 <abdubakora1232@gmail.com>
2026-06-27 07:27:27 +0300

    fix: simplify data loader by removing automatic feature engineering and preprocessing

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,26 +1,21 @@
 import pandas as pd
-from src.features_extraction import add_spectral_type_formula,add_galaxy_population_formula,build_features
 
 def load_data(df_path,include_class=True,is_org_dataset=False):
     df = pd.read_csv(df_path)
-    df = build_features(df)
 
     if is_org_dataset:
         df.drop(['obj_ID','run_ID','rerun_ID','cam_col','field_ID','spec_obj_ID','plate','fiber_ID','MJD'],inplace=True,axis=1)
-        df = add_spectral_type_formula(df)
-        df = add_galaxy_population_formula(df)
-        
     else:
         df.drop('id',inplace=True,axis=1)
 
-    cols = ['spectral_type','galaxy_population']
-    
-    dummy_data = pd.get_dummies(df[cols],dtype=int)
-    dummy_data.drop('galaxy_population_Blue_Cloud',inplace=True,axis=1)
+        cols = ['spectral_type','galaxy_population']
+        
+        dummy_data = pd.get_dummies(df[cols],dtype=int)
+        dummy_data.drop('galaxy_population_Blue_Cloud',inplace=True,axis=1)
 
-    df.drop(cols,inplace=True,axis=1)
+        df.drop(cols,inplace=True,axis=1)
 
-    df = pd.concat([df,dummy_data],axis=1)
+        df = pd.concat([df,dummy_data],axis=1)
 
     if include_class:
         from sklearn.preprocessing import LabelEncoder
