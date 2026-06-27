3891b9a5747c322787c9f8d4441ebb470413b283
abdubakr77 <abdubakora1232@gmail.com>
2026-06-27 05:26:45 +0300

    feat: support loading and preprocessing of the original dataset in the data loader

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,9 +1,17 @@
 import pandas as pd
+from features_extraction import add_spectral_type_formula,add_galaxy_population_formula,build_features
 
-
-def load_data(df_path,include_class=True):
+def load_data(df_path,include_class=True,is_org_dataset=False):
     df = pd.read_csv(df_path)
-    df.drop('id',inplace=True,axis=1)
+    df = build_features(df)
+
+    if is_org_dataset:
+        df.drop(['obj_ID','run_ID','rerun_ID','cam_col','field_ID','spec_obj_ID','plate','fiber_ID','MJD'],inplace=True,axis=1)
+        df = add_spectral_type_formula(df)
+        df = add_galaxy_population_formula(df)
+        
+    else:
+        df.drop('id',inplace=True,axis=1)
 
     cols = ['spectral_type','galaxy_population']
     
